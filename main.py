7798ca16c86aff50f1e8c98dbe9f71c9cf0a7678
sanwas <92679471+sanwas@users.noreply.github.com>
2022-08-06 10:24:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ Name = input("Enter your name: ")
 Height = input("Enter your Height: ")
 Age = input("Enter Your age: ")
 
-print("My name is", name, ". I'm ",Age," Years Old. I'm ",Height," tall.")
+print("My name is", Name, ". I'm ",Age," Years Old. I'm ",Height," tall.")
