ed27487c7061859d59fe04804cf7b0fd00d6d6d9
sanwas <92679471+sanwas@users.noreply.github.com>
2022-08-06 10:05:12 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+Name = input("Enter your name: ")
+Height = input("Enter your Height: ")
+Age = input("Enter Your age: ")
+
+print("My name is", name, ". I'm ",Age," Years Old. I'm ",Height," tall.")
